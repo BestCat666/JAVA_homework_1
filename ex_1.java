72111f6621ba84b4aa2340675a059e931f61bdd0
BestCat <kotova-irina11@rambler.ru>
2023-01-21 15:21:55 +0300

    зачада 1 сумма и произведение 1-n

---
+++ ex_1.java
@@ -0,0 +1,9 @@
+//Вычислить n-ое треугольного число(сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+public class ex_1 {
+    public static void main(String[] args) {
+    var a = 123;
+    System.out.println(a); // 123
+    var d = 123.456;
+    System.out.println(d); // 123.456
+    }
+   }
