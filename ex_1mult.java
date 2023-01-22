44475bba56fe909cb953b1f3ce7340e3cfa87744
BestCat <kotova-irina11@rambler.ru>
2023-01-22 19:39:07 +0300

    зачада 1 произведение чисел от 1 до n

---
+++ ex_1mult.java
@@ -0,0 +1,11 @@
+//n! (произведение чисел от 1 до n)
+public class ex_1mult {
+    public static void main(String[] args) {
+        int num = 8;
+        int fact = 1;
+        for (int i = 1; i <= num; i++) {
+            fact = fact * i;
+        }
+        System.out.println(fact);
+    }
+}
