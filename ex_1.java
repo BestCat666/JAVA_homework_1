7048032237a2a9f2aff3463a50e768731f7472f4
BestCat <kotova-irina11@rambler.ru>
2023-01-22 19:37:51 +0300

    зачада 1 сумма чисел от 1 до n

--- ex_1.java
+++ ex_1.java
@@ -1,9 +1,11 @@
-//Вычислить n-ое треугольного число(сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+//Вычислить n-ое треугольного число(сумма чисел от 1 до n).
 public class ex_1 {
     public static void main(String[] args) {
-    var a = 123;
-    System.out.println(a); // 123
-    var d = 123.456;
-    System.out.println(d); // 123.456
+        int num = 5;
+        int sum = 0;
+        for (int i = 0; i <= num; i++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
     }
-   }
+}
